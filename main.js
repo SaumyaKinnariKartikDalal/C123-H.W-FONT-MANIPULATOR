8f981b1e6c55a225f55fbec3b7805f153634a628
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2023-04-28 21:22:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(300,300);
-    canvas.center();
+    canvas = createCanvas(560, 425);
+    canvas.position(800, 250);
+    video = createCapture(VIDEO);
+    video.size(560, 425);
+    video.position(100, 250)
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses)
 }
 
 function draw(){
+}
 
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+    if (results.length>0) {
+        console.log(results);
+    }
 }
