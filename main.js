1c529ef957c4c60236dde554488682fd18cd2f8f
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2023-05-01 11:39:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftwristx = 0;
+rightwristx = 0;
+difference = 0;
+
 function preload(){
 
 }
@@ -13,6 +17,11 @@ function setup(){
 }
 
 function draw(){
+    background('wheat');
+    fill('#FF0000');
+    text("Saumya Dalal", 10,400);
+    textSize(difference);
+
 }
 
 function modelLoaded(){
@@ -22,5 +31,8 @@ function modelLoaded(){
 function gotPoses(results){
     if (results.length>0) {
         console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+        rightwristx = results[0].pose.rightWrist.x;
+        difference = floor(leftwristx-rightwristx);
     }
 }
